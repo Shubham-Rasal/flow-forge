f483d6e57b0e127f758139572f83fb7fac5447fa
Shubham-Rasal <bluequbits@gmail.com>
2023-12-16 14:33:47 +0530

    Add workspaces and folders tables to schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,1 +1,35 @@
-export {};
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+
+export const workspaces = pgTable('workspaces', {
+    id : uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt : timestamp('created_at' , {
+        withTimezone : true,
+        mode : "string"
+    }),
+    workspaceOwner : uuid('workspace_owner').notNull(),
+    title: text('title').notNull(),
+    iconId : uuid('icon_id').notNull(),
+    data : text('data'),
+    inTrash : text('in_trash'),
+    logo: text('logo'),
+    bannerUrl: text('banner_url'),
+})
+
+
+export const folders = pgTable('folders', {
+    id : uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt : timestamp('created_at' , {
+        withTimezone : true,
+        mode : "string"
+    }),
+    workspaceOwner : uuid('workspace_owner').notNull(),
+    title: text('title').notNull(),
+    iconId : uuid('icon_id').notNull(),
+    data : text('data'),
+    inTrash : text('in_trash'),
+    logo: text('logo'),
+    bannerUrl: text('banner_url'),
+    workspaceId: uuid('workspace_id').notNull().references(() => workspaces.id , {onDelete :"cascade"})
+})
+
