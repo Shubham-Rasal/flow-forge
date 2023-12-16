c7b3122833ffad22f13bcf993634c9b532f3bc86
Shubham-Rasal <bluequbits@gmail.com>
2023-12-16 23:45:53 +0530

    Add middleware for authentication and session management

---
+++ src/middleware.ts
@@ -0,0 +1,55 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  // Create a Supabase client configured to use cookies
+  const supabase = createMiddlewareClient({ req, res} , {
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ANON_KEY,
+  });
+
+  // Refresh session if expired - required for Server Components
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (req.nextUrl.pathname.startsWith("/dashboard") && !session) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  //check for invalid of expired code
+  //Email+link+is+invalid+or+has+expired
+  const emailLinkError = 'Email link is invalid or has expired'
+  if(
+    req.nextUrl.searchParams.get('error_description') === emailLinkError &&
+    req.nextUrl.pathname != '/signup'
+  ) {
+
+    return NextResponse.redirect(new URL(`/signup?error=${emailLinkError}`, req.url));
+
+  }
+
+  //check if session exists for login and signup
+  if (session && (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/signup")) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return res;
+}
+
+// Ensure the middleware is only called for relevant paths.
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
+};
