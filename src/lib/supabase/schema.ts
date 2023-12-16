9f4ff571da23ddf86329069e76e9118bcd2242b7
Shubham-Rasal <bluequbits@gmail.com>
2023-12-16 15:36:26 +0530

    Add new tables and fields to the schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,35 +1,84 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+    boolean,
+    integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus } from "../../../migrations/schema";
 
 
-export const workspaces = pgTable('workspaces', {
-    id : uuid('id').defaultRandom().primaryKey().notNull(),
-    createdAt : timestamp('created_at' , {
-        withTimezone : true,
-        mode : "string"
-    }),
-    workspaceOwner : uuid('workspace_owner').notNull(),
-    title: text('title').notNull(),
-    iconId : uuid('icon_id').notNull(),
-    data : text('data'),
-    inTrash : text('in_trash'),
-    logo: text('logo'),
-    bannerUrl: text('banner_url'),
-})
+export const workspaces = pgTable("workspaces", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  workspaceOwner: uuid("workspace_owner").notNull(),
+  title: text("title").notNull(),
+  iconId: uuid("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+});
 
+export const folders = pgTable("folders", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  workspaceOwner: uuid("workspace_owner").notNull(),
+  title: text("title").notNull(),
+  iconId: uuid("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, { onDelete: "cascade" }),
+});
 
-export const folders = pgTable('folders', {
-    id : uuid('id').defaultRandom().primaryKey().notNull(),
-    createdAt : timestamp('created_at' , {
-        withTimezone : true,
-        mode : "string"
-    }),
-    workspaceOwner : uuid('workspace_owner').notNull(),
-    title: text('title').notNull(),
-    iconId : uuid('icon_id').notNull(),
-    data : text('data'),
-    inTrash : text('in_trash'),
-    logo: text('logo'),
-    bannerUrl: text('banner_url'),
-    workspaceId: uuid('workspace_id').notNull().references(() => workspaces.id , {onDelete :"cascade"})
-})
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }),
+  workspaceOwner: uuid("workspace_owner").notNull(),
+  title: text("title").notNull(),
+  iconId: uuid("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, { onDelete: "cascade" }),
+  folderId: uuid("folder_id")
+    .notNull()
+    .references(() => folders.id, { onDelete: "cascade" }),
+});
+
+export const subscriptions = pgTable("subscriptions", {
+	id: text("id").primaryKey().notNull(),
+	userId: uuid("user_id").notNull(),
+	status: subscriptionStatus("status"),
+	metadata: jsonb("metadata"),
+	priceId: text("price_id").references(() => prices.id),
+	quantity: integer("quantity"),
+	cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+	created: timestamp("created", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+	currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+	currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+	endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).defaultNow(),
+	cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).defaultNow(),
+	canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).defaultNow(),
+	trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).defaultNow(),
+	trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).defaultNow(),
+});
 
