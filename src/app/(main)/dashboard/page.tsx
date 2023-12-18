515cec7968d4c5cc19dd6451dcf78b796e4914fb
Shubham-Rasal <bluequbits@gmail.com>
2023-12-18 15:42:04 +0530

    Refactor dashboard page component

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,27 +1,53 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { LogoutAction } from "@/lib/server-actions/auth-actions";
-import { useRouter } from "next/navigation";
-import React from "react";
+import React from 'react';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
-const DashBoard = () => {
-  const router = useRouter();
+import { cookies } from 'next/headers';
+import db from '@/lib/supabase/db';
+import { redirect } from 'next/navigation';
+import { getUserSubscriptionStatus } from '@/lib/supabase/queries';
+import CreateWorkspace from '@/components/create-workspace';
+import { createWorkspace } from '@/lib/server-actions/workspace-actions';
 
-  const handleLogout = async () => {
-    const { error } = await LogoutAction();
-    if (error) {
-      console.log(error);
-    } else {
-      router.push("/login");
-    }
-  };
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies }, {
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ANON_KEY,
+  });
 
-  return (
-    <div>
-      DashBoard
-      <Button onClick={handleLogout}>Logout</Button>
-    </div>
-  );
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  if (subscriptionError) return;
+
+  
+
+
+  if (!workspace)
+    return (
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold">Welcome {user.email}</h1>
+          <p className="text-lg text-center">
+            You don't have any workspaces yet. Create one to get started.
+          </p>
+        <CreateWorkspace
+          user={user}
+          subscriptionstatus={subscription}
+          createWorkspace={createWorkspace}
+        />
+      </div>
+    );
+
+  redirect(`/dashboard/${workspace.id}`);
 };
 
-export default DashBoard;
+export default DashboardPage;
