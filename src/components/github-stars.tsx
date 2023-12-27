e2083884640145309541b03e7897d67729de2443
Shubham-Rasal <bluequbits@gmail.com>
2023-12-27 14:05:59 +0530

    num to string

--- src/components/github-stars.tsx
+++ src/components/github-stars.tsx
@@ -2,21 +2,19 @@ import { Github } from "lucide-react";
 
 const url = process.env.NEXT_PUBLIC_GITHUB_URL;
 
-function formatNumber(number: number) {
+function formatNumber(number: number): string {
   const absNumber = Math.abs(number);
   if (absNumber >= 1e6) {
     return (number / 1e6).toFixed(1) + "M";
   } else if (absNumber >= 1e3) {
     return (number / 1e3).toFixed(1) + "k";
   } else {
-    return 1;
+    return number.toString();
   }
 }
 
 async function getStars() {
-  const res = await fetch(
-    `https://api.github.com/repos/${url}`
-  );
+  const res = await fetch(`https://api.github.com/repos/${url}`);
   const json = await res.json();
   return formatNumber(json.stargazers_count);
 }
@@ -32,13 +30,11 @@ const GithubStars = async () => {
       rel="noopener noreferrer"
       className="inline-flex items-center space-x-2 p-2 hover:bg-slate-850 rounded-md transition-colors"
     >
-        <Github
-          className="h-6 w-6 text-neutral-800 transition-colors hover:text-neutral-600 dark:text-neutral-200 dark:hover:text-neutral-400"
-          strokeWidth={1.5}
-        />
-        <span className=" text-neutral-800 dark:text-neutral-200">
-          {stars}
-        </span>
+      <Github
+        className="h-6 w-6 text-neutral-800 transition-colors hover:text-neutral-600 dark:text-neutral-200 dark:hover:text-neutral-400"
+        strokeWidth={1.5}
+      />
+      <span className=" text-neutral-800 dark:text-neutral-200">{stars}</span>
     </a>
   );
 };
