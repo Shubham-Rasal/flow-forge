96e93c7c24086630eb9169cf6abd45586dccd9ff
Shubham-Rasal <bluequbits@gmail.com>
2023-12-29 18:28:01 +0530

    Refactor auth-actions.ts file

--- src/lib/server-actions/auth-actions.ts
+++ src/lib/server-actions/auth-actions.ts
@@ -21,36 +21,29 @@ if (NODE_ENV != "production") siteUrl = "http://localhost:3000/builder";
 else siteUrl = NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000/builder";
 
 export async function LoginAction({ email }: z.infer<typeof loginSchema>) {
-  const authClient = createRouteHandlerClient(
-    { cookies },
-    {
-      supabaseKey,
-      supabaseUrl,
-    }
-  );
   const signInResponse = await authClient.auth.signInWithOtp({
     email,
     options: {
       shouldCreateUser: false,
       // emailRedirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/callback`,
       emailRedirectTo: `http://localhost:3000/api/auth/callback`,
-    },  
+    },
   });
 
   return signInResponse;
 }
 
+const authClient = createRouteHandlerClient(
+  { cookies },
+  {
+    supabaseKey,
+    supabaseUrl,
+  }
+);
+
 //signup
 
 export async function SignUpAction({ email }: z.infer<typeof loginSchema>) {
-  const authClient = createRouteHandlerClient(
-    { cookies },
-    {
-      supabaseKey,
-      supabaseUrl,
-    }
-  );
-
   //check if user exists
   const user = await authClient.from("users").select("*").eq("email", email);
   if (user.data?.length) {
@@ -70,14 +63,22 @@ export async function SignUpAction({ email }: z.infer<typeof loginSchema>) {
 
 //logout
 export async function LogoutAction() {
-  const authClient = createRouteHandlerClient(
-    { cookies },
-    {
-      supabaseKey,
-      supabaseUrl,
-    }
-  );
   const response = await authClient.auth.signOut();
 
   return response;
+}
+
+export async function signInWithGithubAction() {
+  const { data, error } = await authClient.auth.signInWithOAuth({
+    provider: "github",
+  });
+
+  return { data, error };
+}
+
+//get current user
+export async function getCurrentUserAction() {
+  const userResponse = await authClient.auth.getUser();
+
+  return userResponse;
 }
